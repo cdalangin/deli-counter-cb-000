036e92c1617d2cb0c4e81229af2477c7db42215d
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-18 22:14:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,11 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   end
+ else
+   current = []
+     array.each_with_index do |people, index|
+       index = index.to_i + 1
+     current << "#{index}. #{people} "
+     end
+  puts "The line is currently:" current.join
 end
